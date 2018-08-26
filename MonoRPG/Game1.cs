f7406382ef596d89ccc76cc071e035c0429ba027
Halim Lais <halimlais@hotmail.com>
2018-08-26 21:53:16 +0100

    Mono Game RPG
    zelda type RPG made on MonoGame

--- MonoRPG/Game1.cs
+++ MonoRPG/Game1.cs
@@ -4,79 +4,107 @@ using Microsoft.Xna.Framework.Input;
 
 namespace MonoRPG
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+    enum direction {down,up,left,right };
+  
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Texture2D playerSprite;
+        Texture2D playerDown;
+        Texture2D playerUp;
+        Texture2D playerLeft;
+        Texture2D playerRight;
+
+        Texture2D eyeEnemySprite;
+        Texture2D snakeEnemySprite;
+
+        Texture2D bushSprite;
+        Texture2D treeSprite;
+
+        Texture2D heartSprite;
+        Texture2D bulletSprite;
+
+        Player player = new Player();
+
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.PreferredBackBufferWidth = 1080;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            
+            
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+       
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            playerSprite = Content.Load<Texture2D>("Player/player");
+            playerDown = Content.Load<Texture2D>("Player/playerDown");
+            playerUp = Content.Load<Texture2D>("Player/playerUp");
+            playerLeft = Content.Load<Texture2D>("Player/playerLeft");
+            playerRight = Content.Load<Texture2D>("Player/playerRight");
+
+            bushSprite = Content.Load<Texture2D>("Obstacles/bush");
+            treeSprite = Content.Load<Texture2D>("Obstacles/tree");
+
+            eyeEnemySprite = Content.Load<Texture2D>("Enemies/eyeEnemy");
+            snakeEnemySprite = Content.Load<Texture2D>("Enemies/snakeEnemy");
+
+            bulletSprite = Content.Load<Texture2D>("Misc/bullet");
+            heartSprite = Content.Load<Texture2D>("Misc/heart");
+
+            player.animation = new AnimatedSprite(playerDown, 1, 4);
+
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+  
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+     
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            player.Update(gameTime);
 
             // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.ForestGreen);
+            player.animation.Draw(spriteBatch, player.Position);
+
+            spriteBatch.Begin();
+            
+
+
+
 
             // TODO: Add your drawing code here
 
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
---
+++ MonoRPG/Player.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoRPG
+{
+    class Player
+    {
+        Vector2 position = new Vector2(100, 100);
+        int speed = 200;
+        int health = 3;
+        bool isMoving = false;
+        direction playerDirection = direction.down;
+
+        public AnimatedSprite animation;
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health = value;
+            }
+        }
+        public Vector2 Position
+        {
+            get {
+                return position;
+            }
+           
+        }
+
+        public void SetX(float x)
+        {
+            position.X = x;
+        }
+        public void SetY(float y)
+        {
+            position.Y = y;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            float deltaTime =  speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            KeyboardState keyInput = Keyboard.GetState();
+
+            animation.Update(gameTime);
+
+            isMoving = false;
+
+            if (keyInput.IsKeyDown(Keys.Right))
+            {
+                playerDirection = direction.right;
+                isMoving = true;
+
+            }
+            if (keyInput.IsKeyDown(Keys.Left))
+            {
+                playerDirection = direction.left;
+                isMoving = true;
+
+            }
+            if (keyInput.IsKeyDown(Keys.Down))
+            {
+                playerDirection = direction.down;
+                isMoving = true;
+
+            }
+            if (keyInput.IsKeyDown(Keys.Up))
+            {
+                playerDirection = direction.up;
+                isMoving = true;
+
+            }
+            if (isMoving)
+            {
+                switch (playerDirection)
+                {
+                    case direction.down:
+                        position.Y += deltaTime;
+                        break;
+                    case direction.up:
+                        position.Y -= deltaTime;
+                        break;
+                    case direction.left:
+                        position.X -= deltaTime;
+                        break;
+                    case direction.right:
+                        position.X += deltaTime;
+                        break;
+                    default:
+
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
